10319bb9d2f1ce8af90e70dc783e88a1c62b4de7
Roshni Patel <roshnipatel@Roshnis-MacBook-Pro.local>
2023-05-06 15:02:07 -0500

    move functions from server.js to routes folder

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const notes_router = require('./notes');
+
+const app = express();
+
+app.use('/notes', notes_router);
+
+module.exports = app;
---
+++ routes/notes.js
@@ -0,0 +1,70 @@
+const app = require('express').Router();
+const { readAndAppend, readFromFile, writeToFile, deleteNote } = require('../helpers/fsHelper');
+const uuid = require('../helpers/uuid')
+
+const fs = require('fs');
+const util = require('util');
+
+// promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+// function to write data to the JSON file given a destination and some content
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+// function to read data from a given a file and append some content
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+// API notes | get route
+app.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+
+// API notes | post route
+app.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  const { title, text } = req.body;
+  if (title && text) {
+    console.log(req.body);
+    // save in object
+    const newNote = {
+      title,
+      text,
+      id: uuid()
+    };
+    // reading and appending db.json file to existing content
+    readAndAppend(newNote, './db/db.json');
+    res.json(`note added successfully!`);
+  } else {
+    res.json('error in adding note')
+  }
+});
+
+app.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((data) => data.id !== noteId);
+      // saving to filesystem
+      writeFile('./db/db.json', result);
+      // response for deleted notes
+      res.json(`The selected note ${noteId} has been deleted 🗑️`);
+    });
+})
+
+module.exports = app;
